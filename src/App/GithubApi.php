6283829af1aed3d7354c35be3ef77db04218c963
Nahid Bin Azhar <nahid.dns@gmail.com>
2017-07-12 01:10:22 +0600

    nothing

--- src/App/GithubApi.php
+++ src/App/GithubApi.php
@@ -11,20 +11,13 @@ class GithubApi extends AbstractApi
 
     protected function setBaseUrl():string
     {
-        return "https://area51.pathao.com/";
+        return "https://domain.com/";
     }
 
 
     public function getOrgRepos()
     {
-        return $this->formParams([
-            'client_id'=>'60b700b76c44ef656f21ebffb07205c1',
-            'client_secret'=>'5c8a5a7802d8814452537203785166c7',
-            'grant_type'=>'password',
-            'username'=>'01882123456@pathao.com',
-            'password'=>'1234',
-            'scope'=>'crud_me'
-        ])->post('login');
+        return $this->post('login');
 
     }
 
@@ -37,7 +30,7 @@ class GithubApi extends AbstractApi
 
     protected function getAccessToken(): string
     {
-        return '4CaYHnlQCCaMPLSvzYQyZXO7KP0X3Bfh0Sn4uEWW';
+        return '';
     }
 
     protected function setDefaultHeaders():array
