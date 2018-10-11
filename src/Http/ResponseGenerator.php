6dc27d3e7e2b63e4e13e0eae6c1a0a66b3516bd9
Nahid Bin Azhar <nahid.dns@gmail.com>
2018-10-11 17:54:41 +0600

    remove piping system

--- src/Http/ResponseGenerator.php
+++ src/Http/ResponseGenerator.php
@@ -6,42 +6,16 @@ abstract class ResponseGenerator
 {
     protected $response;
 
-    protected $pipes = [];
-
-    protected $pipeResponse = null;
-
 
     public function __construct(Response $response)
     {
         $this->response = $response;
-        $this->pipe();
     }
 
-    protected function pipe()
-    {
-        $param = [];
-        foreach($this->pipes as $pipe) {
-            $method = 'pipe' . ucfirst($pipe);
-
-            if (method_exists($this, $method)) {
-                if (count($param) == 0) {
-                    $param = $this->response;
-                }
-
-                $this->pipeResponse = call_user_func_array([$this, $method], [$param]);
-                $param = $this->pipeResponse;
-            }
-        }
-    }
 
     protected function jsonq()
     {
         $response = $this->response;
         return $response();
-    }
-
-    public function getPipedData()
-    {
-        return $this->pipeResponse;
     }
 }
