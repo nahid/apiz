d856ecf2341ed3ef5c973486a19912730e72e236
Nahid Bin Azhar <nahid.dns@gmail.com>
2019-10-01 01:00:57 +0600

    added: codedoc for AbstractApi class magic method

--- src/AbstractApi.php
+++ src/AbstractApi.php
@@ -7,6 +7,17 @@ use GuzzleHttp\Psr7\Request as Psr7Request;
 use Apiz\Http\Request;
 use Apiz\Http\Response;
 
+/**
+ * Class AbstractApi
+ * @package Apiz
+ *
+ * @method \Apiz\Http\Response post(string $uri)
+ * @method \Apiz\Http\Response get(string $uri)
+ * @method \Apiz\Http\Response put(string $uri)
+ * @method \Apiz\Http\Response patch(string $uri)
+ * @method \Apiz\Http\Response delete(string $uri)
+ * @method \Apiz\Http\Response options(string $uri)
+ */
 abstract class AbstractApi
 {
     /**
@@ -487,6 +498,9 @@ abstract class AbstractApi
         }
 
         $responder = $this->response;
+        /**
+         * @var $resp Response
+         */
         $resp = new $responder($response, $request);
 
         if (is_null($this->response) || !($resp instanceof Response)) {
