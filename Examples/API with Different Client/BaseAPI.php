df28839f9317c3fa2ad26fdcbf16df1c42099289
rsayed007 <romansyed007@gmail.com>
2022-04-04 15:35:19 +0600

    add an example for api default header validaion with getDefaultHeaders() function

--- Examples/API with Different Client/BaseAPI.php
+++ Examples/API with Different Client/BaseAPI.php
@@ -26,4 +26,33 @@ abstract class BaseAPI extends AbstractApi
     {
         return 'api';
     }
+
+    /**
+     * get default headers that will automatically bind with every request headers
+     *
+     * @return array
+     */
+
+
+    /**
+     * some time we need check API  authentication we can check basic 
+     * and token base authentication with getDefaultHeaders function 
+     * 
+     * token base authentication 
+     * return [
+     *          'access_token' => $_ENV['ACCESS_TOKEN'] 
+     *      ];
+     * 
+     * Basic authentication   
+     * return [
+     *          'Authorization' => 'Basic ' . base64_encode('API_AUTH_USER' . ':' . 'API_AUTH_PASS'),
+     *      ];
+     */
+
+    protected function getDefaultHeaders()
+    {
+        return [
+            'Authorization' => 'Basic ' . base64_encode(env('YOUR_API_AUTH_USER_NAME') . ':' . env('YOUR_API_AUTH_PASSWORD')),
+        ];
+    }
 }
